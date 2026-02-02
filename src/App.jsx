2f63db4ce385d4b3f2b4faeb627d814592bd420c
Julian <jmueller956@gmail.com>
2026-02-01 21:40:57 -0600

    Fixed tailwind CDN typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import HomeScreen from "./components/homeScreen";
 
 function App() {
--- src/components/homeScreen.jsx
+++ src/components/homeScreen.jsx
@@ -3,7 +3,20 @@ import React from "react";
 function HomeScreen({ onPlay }) {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-blue-500 to-indigo-600 text-white px-4 text-center" >
-            
+            <h1>The Grapes of ROTH</h1>
+            <h2>The ultimate ROTH IRA management simulator. </h2>
+            <p>
+                Start as a 22 year old and choose how to fund and grow your ROTH IRA.
+                Will you be retirement ready?   
+            </p>
+            <button
+                onClick={onPlay}
+                className="bg-white text-blue-600 font-bold px-8 py-4 rounded-lg shadow-lg hover:scale-105 transform transition duration-300"
+            >
+            Play
+            </button>
         </div>
-    )
+    );
 }
+
+export default HomeScreen;
