aa0db8d95c5ad74a0dbc6fd549e10e8cd25e7dcb
Julian <jmueller956@gmail.com>
2026-02-01 22:00:35 -0600

    Added vite testing env

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "../grapes-of-roth/src/App.jsx";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
