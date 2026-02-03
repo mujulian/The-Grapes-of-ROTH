a219d5737650e60d4409c153fdf5327fffe8bc77
Julian <jmueller956@gmail.com>
2026-02-02 21:57:15 -0600

    color palette change

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 function HomeScreen({ onPlay }) {
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-blue-500 to-indigo-600 text-white px-4 text-center" >
-            <h1>The Grapes of ROTH</h1>
+        <div className="flex flex-col items-center justify-center h-screen 
+                        bg-gradient-to-b from-[#240952] via-[#240952] to-[#6622CC] text-white px-4 text-center" >
+            <h1 className="text-6xl md:text-7xl font-black text-[#F0F4EF] ">
+                The Grapes of{" "}
+            <span className="font-black text-[#A755C2]">
+                ROTH
+            </span>
+            </h1>
             <h2>The ultimate ROTH IRA management simulator. </h2>
             <p>
                 Start as a 22 year old and choose how to fund and grow your ROTH IRA.
@@ -11,7 +17,8 @@ function HomeScreen({ onPlay }) {
             </p>
             <button
                 onClick={onPlay}
-                className="bg-white text-blue-600 font-bold px-8 py-4 rounded-lg shadow-lg hover:scale-105 transform transition duration-300"
+                className="bg-white text-blue-600 font-bold px-8 py-4 rounded-lg shadow-lg 
+                           hover:scale-105 transform transition duration-300"
             >
             Play
             </button>
